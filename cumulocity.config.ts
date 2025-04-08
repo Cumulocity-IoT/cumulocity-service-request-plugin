3b7cb87287967e3f172f50a3bfbac2a3a6d7f159
Christian Guether <christian.guether@cumulocity.com>
2025-04-08 09:51:59 +0200

    update configuration

--- cumulocity.config.ts
+++ cumulocity.config.ts
@@ -1,5 +1,5 @@
 import type { ConfigurationOptions } from '@c8y/devkit';
-import { author, description, version, name } from './package.json';
+import { author, description, version, name, license } from './package.json';
 
 export default {
   runTime: {
@@ -7,6 +7,7 @@ export default {
     description,
     version,
     name,
+    license,
     contentSecurityPolicy:
       "base-uri 'none'; default-src 'self' 'unsafe-inline' http: https: ws: wss:; connect-src 'self' http: https: ws: wss:;  script-src 'self' *.bugherd.com *.twitter.com *.twimg.com *.aptrinsic.com 'unsafe-inline' 'unsafe-eval' data:; style-src * 'unsafe-inline' blob:; img-src * data: blob:; font-src * data:; frame-src *; worker-src 'self' blob:;",
     dynamicOptionsUrl: true,
